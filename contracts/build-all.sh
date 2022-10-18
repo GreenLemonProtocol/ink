ebb7e81d502e4a0f928e619344c71e21647c5243
wuyahuang <wuyahuang@gmail.com>
2022-10-18 18:42:10 +0800

    remove specific version of +nightly

--- contracts/build-all.sh
+++ contracts/build-all.sh
@@ -2,7 +2,11 @@
 
 set -eu
 
-cargo +nightly-2022-08-15 contract build --manifest-path relayer/Cargo.toml
-cargo +nightly-2022-08-15 contract build --manifest-path verifier/Cargo.toml
-cargo +nightly-2022-08-15 contract build --manifest-path erc721/Cargo.toml
+# cargo +nightly-2022-08-15 contract build --manifest-path relayer/Cargo.toml
+# cargo +nightly-2022-08-15 contract build --manifest-path verifier/Cargo.toml
+# cargo +nightly-2022-08-15 contract build --manifest-path erc721/Cargo.toml
+
+cargo +nightly contract build --manifest-path relayer/Cargo.toml
+cargo +nightly contract build --manifest-path verifier/Cargo.toml
+cargo +nightly contract build --manifest-path erc721/Cargo.toml
 
