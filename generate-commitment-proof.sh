6c11a967dc683569a1e5719a4dad00f634a37fd4
wuyahuang <wuyahuang@gmail.com>
2022-11-17 13:26:30 +0800

    add generate-commitment-proof.sh

---
+++ generate-commitment-proof.sh
@@ -0,0 +1,4 @@
+node scripts/0-generateCommitment.js
+node scripts/1-compute-witness.js
+node scripts/2-generate-proof.js
+node scripts/3-verify-proof-offchain.js
--- proof.sh
+++
@@ -1,4 +0,0 @@
-node tests/1-generateCommitment.js
-node tests/2-compute-witness.js
-node tests/3-generate-proof.js
-node tests/4-verify-proof-offchain.js
